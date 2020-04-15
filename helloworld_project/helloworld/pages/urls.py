374971f3103e7cc8fc86aabbd8b59d127cc4a8b0
harshOS <ng.harshith579@gmail.com>
2020-04-15 08:47:14 +0530

    added one more url

--- helloworld_project/helloworld/pages/urls.py
+++ helloworld_project/helloworld/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import homePageView
+from .views import sayHi
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', homePageView, name='home'),
+    path('hi', sayHi, name='hai'),
+
 ]
--- helloworld_project/helloworld/pages/views.py
+++ helloworld_project/helloworld/pages/views.py
@@ -2,5 +2,9 @@
 from django.http import HttpResponse
 
 # Create your views here.
+def sayHi(request):
+    return HttpResponse('<h1>Hai buddy<h1>')
+    
 def homePageView(request):
     return HttpResponse('Hello world!')
+
