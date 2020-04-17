d353033879d6a787d77ecb0c913380732ff1e081
harshOS <harshithngr@gmail.com>
2020-04-17 15:06:49 +0530

    adder header

--- pages_project/pages/views.py
+++ pages_project/pages/views.py
@@ -3,5 +3,10 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
