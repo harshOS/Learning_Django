84503bb6a2b2d48af0a359304cadf7ec18f6ca4c
harshOS <harshithngr@gmail.com>
2020-07-09 20:36:03 +0530

    message board app

---
+++ msgboard/msgboard/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for msgboard project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msgboard.settings')
+
+application = get_wsgi_application()
