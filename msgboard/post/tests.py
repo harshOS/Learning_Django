b820ccfc41026beba69fc6f113af7e7d86602f3d
harshOS <harshithngr@gmail.com>
2020-07-11 22:06:16 +0530

    bl\og_app

--- msgboard/post/tests.py
+++ msgboard/post/tests.py
@@ -8,7 +8,7 @@ class PostModelTest(TestCase):
         Post.objects.create(text='just a test')
     
     def test_text_content(self):
-        post=Post.objects.get(id=1)
+        post = Post.objects.get(id=1)
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
 
