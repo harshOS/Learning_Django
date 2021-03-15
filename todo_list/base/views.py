ef149385c06706f861990d567f474f358de1f9e2
harshOS <harshithngr@gmail.com>
2021-03-15 19:08:50 +0530

    added templates

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from  .models import Task
 
 # Create your views here.
 
 class TaskList(ListView):
+    context_object_name = 'tasks'
+    model = Task
+
+class TaskDetail(DetailView):
     model = Task
