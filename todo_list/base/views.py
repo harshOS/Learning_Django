dfbdaabb8470c22c54b52dd72d6eb0908d23aced
harshOS <harshithngr@gmail.com>
2021-03-15 20:50:29 +0530

    addded create task

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from  .models import Task
 
 # Create your views here.
@@ -10,4 +12,11 @@ class TaskList(ListView):
     model = Task
 
 class TaskDetail(DetailView):
+    context_object_name = 'task'
+    template_name = 'base/task.html'
     model = Task
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
