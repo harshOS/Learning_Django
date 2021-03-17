1b29e928a6851393e0bead6e824e05dc5842aa2c
harshOS <harshithngr@gmail.com>
2021-03-17 14:12:20 +0530

    added access only to user specific data

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -23,6 +23,13 @@ class TaskList(LoginRequiredMixin,ListView):
     context_object_name = 'tasks'
     model = Task
 
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks']= context['tasks'].filter(user = self.request.user)
+        context['count']= context['tasks'].filter(complete = False)
+        return context
+
+
 class TaskDetail(LoginRequiredMixin,DetailView):
     context_object_name = 'task'
     template_name = 'base/task.html'
@@ -30,12 +37,16 @@ class TaskDetail(LoginRequiredMixin,DetailView):
 
 class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title','description','complete']
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(LoginRequiredMixin,DeleteView):
